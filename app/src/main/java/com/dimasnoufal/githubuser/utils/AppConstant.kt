f5252ea966a74294712364c538ec6754d04b250a
Dimasnoufal <dimasnoufal26@gmail.com>
2023-04-23 20:00:18 +0700

    Update Id

--- app/src/main/java/com/dimasnoufal/githubuser/utils/AppConstant.kt
+++ app/src/main/java/com/dimasnoufal/githubuser/utils/AppConstant.kt
@@ -2,7 +2,7 @@ package com.dimasnoufal.githubuser.utils
 
 object AppConstant {
     const val BASE_URL = "https://api.github.com/"
-    const val API_TOKEN = "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"
+    const val API_TOKEN = "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"
     const val EXTRA_USER = "user"
     const val DEFAULT_TIMER = 3000L
     const val USER_TABLE_NAME = "github_user"
