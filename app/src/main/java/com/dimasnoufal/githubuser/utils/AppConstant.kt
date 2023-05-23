f0639537cb02f1bd0682e008b4b28a9478a6e620
dimasnoufal <dimasnoufal26@gmail.com>
2023-05-23 19:52:28 +0700

    Update API Token

--- app/src/main/java/com/dimasnoufal/githubuser/utils/AppConstant.kt
+++ app/src/main/java/com/dimasnoufal/githubuser/utils/AppConstant.kt
@@ -2,7 +2,7 @@ package com.dimasnoufal.githubuser.utils
 
 object AppConstant {
     const val BASE_URL = "https://api.github.com/"
-    const val API_TOKEN = "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"
+    const val API_TOKEN = "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"
     const val EXTRA_USER = "user"
     const val DEFAULT_TIMER = 3000L
     const val USER_TABLE_NAME = "github_user"
